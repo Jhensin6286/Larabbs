703337744ce5b9d78e78e310e42492bd5bf4e1ab
vagrant <vagrant@homestead>
2018-08-08 13:43:45 +0000

    Finish basic page layout

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,1 +1,11 @@
-require_once __DIR__ . '/helpers.php';
+<?php
+/**
+ * 自定义辅助函数放在这里
+ */
+
+
+
+
+function route_class(){
+    return str_replace('.','-',Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            作者： <a href="https://github.com/Jhensin6286" target="_blank">Jhensin</a><span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:cspxxc@qq.com">联系我们</a></p>
+    </div>
+</footer>
