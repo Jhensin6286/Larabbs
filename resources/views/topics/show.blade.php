228d63d8b94842001132a22b564b029cf4a8c3b4
vagrant <vagrant@homestead>
2018-08-13 09:01:08 +0000

    User can create reply

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -66,14 +66,12 @@
     </div>
 
 
-{{-- 用户回复列表 --}}
+        {{-- 用户回复列表 --}}
         <div class="panel panel-default topic-reply">
             <div class="panel-body">
-                @include('topics._reply_box', ['topic' => $topic])
+                @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
                 @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
             </div>
         </div>
-
-    </div>
 </div>
 @stop
