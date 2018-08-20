75df1f8d3b82c39123dd7c4f1d3bbbfa239a71d2
vagrant <vagrant@homestead>
2018-08-20 10:26:21 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             作者： <a href="https://github.com/Jhensin6286" target="_blank">Jhensin</a><span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:cspxxc@qq.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
